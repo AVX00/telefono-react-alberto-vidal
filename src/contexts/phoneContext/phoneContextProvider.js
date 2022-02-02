625817f8215f151c96c80fdc3a471bdd1e31cb86
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-02 21:30:03 +0100

    :sparkles: added display component

--- src/contexts/phoneContext/phoneContextProvider.js
+++ src/contexts/phoneContext/phoneContextProvider.js
@@ -3,6 +3,7 @@ import PhoneContext from "./phoneContext";
 const PhoneContextProvider = ({ children }) => {
   const data = {
     isCalling: false,
+    number: 99999999,
   };
 
   return <PhoneContext.Provider value={data}>{children}</PhoneContext.Provider>;
