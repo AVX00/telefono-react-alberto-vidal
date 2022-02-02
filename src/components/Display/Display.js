d8bd3e161bf4f9f583946454beb1bfcad95b0833
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-02 23:23:18 +0100

    :bug: fixed useContext import path

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import PhoneContext from "../../contexts/phoneContext/phoneContext";
 
 const Display = () => {
